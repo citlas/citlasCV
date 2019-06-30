96676d79715da5fa9da2f245865a9c617e54d959
Citlalli Prado <citlalli.prado@gmail.com>
2019-06-30 23:05:36 +0200

    Centered contact

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -10,6 +10,15 @@ export class MenuComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    window.onscroll = function() {scrollFunction()};
+
+    function scrollFunction() {
+      if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("navbar").style.top = "0";
+      } else {
+        document.getElementById("navbar").style.top = "-50px";
+      }
+    }
   }
 
 }
